1a6e8a47b33ee950d1aa29a260955ea259da94d0
Tales Costa <talescostam@gmail.com>
2023-04-06 00:05:49 -0300

    upgrade on Controller

--- src/main/java/com/psy/duck/controller/DuckController.java
+++ src/main/java/com/psy/duck/controller/DuckController.java
@@ -5,14 +5,10 @@ import com.psy.duck.repository.DogRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,7 +16,7 @@ public class DuckController {
 
     private final DogRepository dogRepository;
 
-        public DuckController(DogRepository dogRepository) {
+    public DuckController(DogRepository dogRepository) {
                 this.dogRepository = dogRepository;
     }
 
@@ -36,19 +32,66 @@ public class DuckController {
         return ResponseEntity.ok(dogOptional.get());
     }
 
+    @GetMapping(value = "/duck/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Dog>> getAllDucks() {
+
+        List<Dog> dogList = dogRepository.findAll();
+
+        if (dogList.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "dog.not_found_error", null);
+        }
+
+        return ResponseEntity.ok(dogList);
+    }
+
+    @GetMapping(value = "/duck/all/{age}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Dog>> getAllDucksByAge(@PathVariable Integer age) {
+
+        List<Dog> dogListById = dogRepository.findAllByAge(age);
+
+        if (dogListById.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "dog.not_found_error", null);
+        }
+
+        return ResponseEntity.ok(dogListById);
+    }
+
     @PostMapping(value = "/duck", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Dog> saveDuck(@RequestBody Dog newDog){
 
         Dog dog = dogRepository.save(newDog);
-                return ResponseEntity.ok(dog);
+
+        return ResponseEntity.ok(dog);
     }
 
     @PutMapping(value = "/duck/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Dog> replaceDuck(@RequestBody Dog newDog, @PathVariable Long id){
 
-        Dog dog = dogRepository.save(newDog);
-        return ResponseEntity.ok(dog);
+        Optional<Dog> dogOptional = dogRepository.findById(id);
 
+        if (dogOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "dog.not_found_error", null);
+        }
+
+        Dog dog = dogOptional.get();
+        dog.setAge(newDog.getAge());
+        dog.setName(newDog.getName());
+        Dog save = dogRepository.save(dog);
+
+        return ResponseEntity.ok(save);
+
+    }
+
+    @DeleteMapping(value = "/duck/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> deleteDuck(@PathVariable long id) {
+         Optional<Dog> dogOptional = dogRepository.findById(id);
+
+        if (dogOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "dog.not_found_error", null);
+        }
+
+        dogRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
---
+++ src/test/java/com/psy/duck/controller/DuckControllerTest.java
@@ -0,0 +1,19 @@
+package com.psy.duck.controller;
+
+import com.psy.duck.repository.DogRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class DuckControllerTest {
+
+    @Autowired
+    private DogRepository dogRepository;
+
+    @Test
+    public void test
+
+
+}
